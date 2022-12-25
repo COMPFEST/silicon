6e8d1da56683f9c1f2d5dee69a53246b8b529147
DJaegerScript <adjiedjaka0902@gmail.com>
2022-12-25 18:16:08 +0700

    linter leftover

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -5,38 +5,39 @@ import { ButtonProps } from './interface'
 /**
  * Primary UI component for user interaction
  */
-const Button: React.FC<ButtonProps & React.ComponentPropsWithoutRef<'button'>> =
-  ({
-    variant = 'primary',
-    size = 'regular',
-    disabled = false,
-    isLoading = false,
-    leftIcon,
-    rightIcon,
-    children,
-    ...props
-  }) => {
-    return (
-      <StyledButton
-        variant={variant}
-        size={size}
-        disabled={isLoading || disabled}
-        {...props}
-      >
-        {isLoading ? (
-          <StyledLoadingIcon>
-            <LoadingIcon />
-          </StyledLoadingIcon>
-        ) : (
-          <>
-            {leftIcon}
-            {children}
-            {rightIcon}
-          </>
-        )}
-      </StyledButton>
-    )
-  }
+const Button: React.FC<
+  ButtonProps & React.ComponentPropsWithoutRef<'button'>
+> = ({
+  variant = 'primary',
+  size = 'regular',
+  disabled = false,
+  isLoading = false,
+  leftIcon,
+  rightIcon,
+  children,
+  ...props
+}) => {
+  return (
+    <StyledButton
+      variant={variant}
+      size={size}
+      disabled={isLoading || disabled}
+      {...props}
+    >
+      {isLoading ? (
+        <StyledLoadingIcon>
+          <LoadingIcon />
+        </StyledLoadingIcon>
+      ) : (
+        <>
+          {leftIcon}
+          {children}
+          {rightIcon}
+        </>
+      )}
+    </StyledButton>
+  )
+}
 
 const LoadingIcon = () => (
   <svg
