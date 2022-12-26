182444a8cface2da6132328fb69db728845f5263
gibranoderenz <gibranoderenz123@gmail.com>
2022-12-27 01:24:22 +0700

    Add line height

--- src/TipCard/TipCard.style.tsx
+++ src/TipCard/TipCard.style.tsx
@@ -3,13 +3,14 @@ import styled from 'styled-components'
 export const TipCardContainer = styled.div`
   font-family: 'Inter', sans-serif;
   color: #ffffff;
-  font-weight: 600;
+  font-weight: 500;
   border-radius: 0.75rem;
   display: table;
   width: fit-content;
   background-color: #373951;
   border: 1px solid rgba(255, 255, 255, 0.1);
   overflow: hidden;
+  line-height: 150%;
 `
 
 export const Leading = styled.div`
@@ -21,6 +22,8 @@ export const Leading = styled.div`
 `
 
 export const Content = styled.div`
+  display: table-cell;
+  vertical-align: middle;
   max-width: 30ch;
   word-wrap: break-word;
   padding: 0.75rem 1rem;
