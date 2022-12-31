ab2a7e1c1c4d16af7d538564f1e0b36ed42f0f43
gibranoderenz <gibranoderenz123@gmail.com>
2022-12-31 18:12:30 +0700

    Make maximum lines to 2

--- src/TipCard/TipCard.style.tsx
+++ src/TipCard/TipCard.style.tsx
@@ -21,10 +21,18 @@ export const Leading = styled.div`
   border-radius: 0.75rem;
 `
 
+// Reference: https://stackoverflow.com/questions/49350396/how-to-make-a-div-have-2-rows-max-lines-and-get-ellipsis
 export const Content = styled.div`
-  display: table-cell;
+  display: -webkit-box;
+  display: -moz-box;
   vertical-align: middle;
   max-width: 30ch;
+  max-height: 36px;
+  overflow: hidden;
+  text-overflow: ellipsis;
   word-wrap: break-word;
   padding: 0.75rem 1rem;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  -moz-line-clamp: 2;
 `
