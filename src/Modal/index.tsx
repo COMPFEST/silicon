4b388bcebb082072f396427886fd9860f40536ca
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-01-05 20:42:33 +0700

    Modal minus component

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,24 +1,25 @@
-import { ModalProps } from "./interface";
-import React from "react";
-import { ModalContainter, ModalContent } from "./Modal.style";
+import { ModalProps } from './interface'
+import React from 'react'
+import { ModalContainter, ModalContent } from './Modal.style'
 
 const Modal: React.FC<ModalProps> = ({
-    title = '',
-    content = '',
-    imageUrl,
-    acceptButton = null,
-    cancelButton,
-    isDisplayed = false,
-    ...props
+  title = '',
+  content = '',
+  imageUrl,
+  acceptButton = null,
+  cancelButton,
+  isDisplayed = false,
+  ...props
 }) => {
-    return (<ModalContainter>
-        <ModalContent
-        isDisplayed={isDisplayed}>
-            <p>asdas</p>
-            {acceptButton}
-            {cancelButton}
-        </ModalContent>
-    </ModalContainter>)
+  return (
+    <ModalContainter>
+      <ModalContent isDisplayed={isDisplayed}>
+        <p>asdas</p>
+        {acceptButton}
+        {cancelButton}
+      </ModalContent>
+    </ModalContainter>
+  )
 }
 
 export default Modal
