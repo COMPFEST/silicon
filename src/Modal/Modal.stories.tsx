915cb3a5e584fe411ecefa3f9b5be3814c666fe0
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-01-05 20:42:33 +0700

    Modal minus component

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -4,26 +4,24 @@ import Modal from '.'
 import Button from '../Button'
 
 export default {
-    id: 'modal',
-    title: 'Modal',
-    component: Modal,
-    argTypes: {
-        isDisplayed: {
-            control: 'boolean',
-        },
+  id: 'modal',
+  title: 'Modal',
+  component: Modal,
+  argTypes: {
+    isDisplayed: {
+      control: 'boolean',
     },
+  },
 } as ComponentMeta<typeof Modal>
 
-const Template: ComponentStory<typeof Modal> = (args) => (
-    <Modal {...args}/>
-)
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />
 
 export const Displayed = Template.bind({})
 
 Displayed.args = {
-    title: 'test',
-    content: 'test',
-    imageUrl: 'asdsa',
-    acceptButton:  <Button>Accept</Button>,
-    cancelButton: <Button variant='secondary'>Cancel</Button>,
+  title: 'test',
+  content: 'test',
+  imageUrl: 'asdsa',
+  acceptButton: <Button>Accept</Button>,
+  cancelButton: <Button variant="secondary">Cancel</Button>,
 }
--- src/Modal/interface.ts
+++ src/Modal/interface.ts
@@ -1,14 +1,13 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react'
 
 export interface isDisplayedProps {
-  isDisplayed : true | false
+  isDisplayed: true | false
 }
 
 export interface ModalProps extends Partial<isDisplayedProps> {
-    title: string
-    content: string
-    imageUrl?: string
-    acceptButton: ReactNode
-    cancelButton? : ReactNode
-  }
-  
+  title: string
+  content: string
+  imageUrl?: string
+  acceptButton: ReactNode
+  cancelButton?: ReactNode
+}
