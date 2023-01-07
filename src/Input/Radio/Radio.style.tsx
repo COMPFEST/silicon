ccd0d79e87edcfd957a167e02097ad96c4cc6aef
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-01-07 19:39:49 +0700

    change radio cursor on hover

--- src/Input/Radio/Radio.style.tsx
+++ src/Input/Radio/Radio.style.tsx
@@ -61,6 +61,7 @@ export const RadioLabel = styled.div`
   color: white;
   font-size: 16px;
   line-height: 18px;
+  cursor: default;
 
   @media only screen and (max-width: 768px) {
     font-size: 12px;
