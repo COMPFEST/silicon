bda1d07334ad5452d7b0d52e98938de228cdef32
aidahputri <aidahptr15@gmail.com>
2023-01-08 22:53:31 +0700

    add styles

---
+++ src/File Input/FileInput.style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const FileInputContainer = styled.div`
+  width: 588px;
+  height: 260x;
+  outline-style: dotted;
+  outline-color: rgba(255, 255, 255, 0.1);
+  border-radius: 24px; 
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+`
+
+export const Dropzone = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+`
+
+export const LinkInput = styled.div`
+
+`
