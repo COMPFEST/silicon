e1715ee2969753d479f04f0d557a5990e7994363
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2023-01-18 12:44:54 +0700

    Implemented toast hook

--- src/Toast/Toast.stories.tsx
+++ src/Toast/Toast.stories.tsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
 import { ToastProps } from './interface'
-import Toast from '.'
+import useToast from '.'
+import Toast from './toast';
 
 export default {
-    id: 'Toast',
-    title: 'Toast',
-    component: Toast,
+  id: 'toast',
+  title: 'Toast',
+  component: Toast,
 } as Meta
 
-const Template: Story<ToastProps> = (args) => <Toast {...args} />
+const Demo = (props:ToastProps) => {
+    return (
+        <div>
+            <button onClick={useToast({...props})}>Tap me!</button>
+            <Toast position='top-left'/>
+        </div>
+    );
+};
+
+const Template: Story<ToastProps> = (args) => <Demo {...args} />
 
 const myPromise = new Promise(function (myResolve, myReject) {
-    const x = Math.floor(Math.random() * 10);
+  const x = Math.floor(Math.random() * 10)
 
-    setTimeout(() => {
-        if (x % 2 == 0) {
-            myResolve(x);
-        } else {
-            myReject(x);
-        }
-    }, 5000)
-});
+  setTimeout(() => {
+    if (x % 2 == 0) {
+      myResolve(x)
+    } else {
+      myReject(x)
+    }
+  }, 5000)
+})
 
-export const Default = Template.bind({})
-Default.args = {
-    successMessage: 'Success! You got even number',
-    errorMessage: 'Oops.. You got odd number',
-    loadingMessage: 'Loading...',
-    promise: myPromise,
+export const Example = Template.bind({})
+Example.args = {
+  successMessage: 'Success! You got even number',
+  errorMessage: 'Oops.. You got odd number',
+  loadingMessage: 'Loading...',
+  promise: myPromise,
 }
