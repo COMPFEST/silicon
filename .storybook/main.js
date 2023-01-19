f9d1e6c3769170c0eab52ae74be7ab346a46dc5f
haekalpks <hkalipaksi@outlook.com>
2023-01-19 23:10:21 +0700

    init test-runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,9 @@ module.exports = {
     '@storybook/addon-a11y',
     '@storybook/addon-coverage',
     '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
+
   ],
   staticDirs: [{ from: '../public/fonts', to: '/static/media' }],
   framework: '@storybook/react',
