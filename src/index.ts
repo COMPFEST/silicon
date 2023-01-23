752721069a6fa1e020707935dd39ba7b46aa217d
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2023-01-23 20:20:27 +0700

    Simplified toast hook

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import Button from './Button'
 import FileInput from './File-Input'
 import Modal from './Modal'
-import Toast from './Toast/trigger'
-import useToast from './Toast'
 import TextInput from './Input/TextInput'
 import SearchInput from './Input/Search'
 import Radio from './Input/Radio'
 import Checkbox from './Input/Checkbox'
 import Countdown from './Countdown'
 import Disclosure from './Disclosure'
+import { Toast, useToast } from './Toast'
 
 export {
   Button,
