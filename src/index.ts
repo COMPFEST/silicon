569d6278cdf73f3ca304aa5398b6690750569bb4
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-01-25 10:54:43 +0700

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import Button from './Button'
+import TextInput from './Input/TextInput'
+import SearchInput from './Input/Search'
+import Radio from './Input/Radio'
+import Checkbox from './Input/Checkbox'
 
-export { Button }
+export { Button, TextInput, SearchInput, Radio, Checkbox }
