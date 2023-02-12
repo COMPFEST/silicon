eb54c6ff63e0b48dcdc893e4d23b4211cc4a7c4f
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-02-12 22:37:30 +0700

    fix error

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,12 +7,10 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@storybook/addon-coverage',
-
   ],
   staticDirs: [{ from: '../public/fonts', to: '/static/media' }],
   framework: '@storybook/react',
   features: {
     interactionsDebugger: true,
-  }, 
-
+  },
 }
