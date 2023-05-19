d02ce4439850a86e846cb21e63d72800b8d8e44a
IrPanTech <irfannkamill@gmail.com>
2023-05-20 00:20:57 +0700

    make disclosure able to switch theme

--- src/Disclosure/interface.ts
+++ src/Disclosure/interface.ts
@@ -3,4 +3,7 @@ import { ReactNode } from 'react'
 export interface DisclosureProps {
   title: string
   children: ReactNode
+  iconColor?: string
+  titleContainerClassName?: string
+  childrenContainerClassName?: string
 }
